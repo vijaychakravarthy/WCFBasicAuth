d0061fcf591dc4c5f801d17ee5a607cedc3dc364
vijaychakravarthy <vijaychakravarthy.d@gmail.com>
2017-11-14 11:27:00 +0530

    Add files via upload

---
+++ UPValidator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IdentityModel.Selectors;
+
+namespace BasicAuthWCF
+{
+    public class UPValidator : UserNamePasswordValidator
+    {
+        public override void Validate(string userName, string password)
+        {
+            if (!string.IsNullOrEmpty(userName))
+                return;
+        }
+    }
+}
